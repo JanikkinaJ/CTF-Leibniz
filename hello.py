6c32934b792fe9d92ea32843ba011ae5d301641b
kineye <27766253+JanikkinaJ@users.noreply.github.com.>
2026-02-13 09:41:27 +0100

    added post request handling

--- hello.py
+++ hello.py
@@ -1,10 +1,35 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return send_from_directory('static', 'index.html')
 
-@app.route('/<path:filename>')
+@app.route('/admin.html', methods=['GET', 'POST'])
+def login_post():
+   if request.method == 'POST':
+        return login_logic()
+   else:
+        return render_template('admin.html', error=None)
+
+@app.route('/<filename>')
 def serve_file(filename):
     return send_from_directory('static', filename)
+
+def login_logic():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return "valid login :D"
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('admin.html', error=error)
+
+def valid_login(name, password):
+    if name == "admin" and password == "password":
+        return True
+    return False
+
